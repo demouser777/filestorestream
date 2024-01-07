03bcdc3a7082653335de2f827402a3a98e738ff8
demouser777 <116957723+demouser777@users.noreply.github.com>
2024-01-07 23:41:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -94,9 +94,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "furious-cami-filestore12.koyeb.app"
+        URL = "https://furious-cami-filestore12.koyeb.app"
     else:
-        URL = "furious-cami-filestore12.koyeb.app"
+        URL = "https://furious-cami-filestore12.koyeb.app"
 
 
 
